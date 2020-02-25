f07affbac2b811f3bac0378b702d17c50a21a743
Zanetta Norris <znorris@zipcodes-MBP-2.localdomain>
2020-02-25 17:18:47 -0500

    added field and method stubs to Dealer class

--- src/main/java/io/zipcoder/casino/Player/Dealer.java
+++ src/main/java/io/zipcoder/casino/Player/Dealer.java
@@ -1,4 +1,17 @@
 package io.zipcoder.casino.Player;
 
+import com.sun.org.apache.xpath.internal.objects.XNumber;
+import io.zipcoder.casino.GameClasses.CardGames.Card;
+
 public class Dealer {
+    String Name;
+    Integer idNumber;
+
+    Integer balance;
+    Integer age;
+
+    public Card dealCard (Integer numberOfCardsToDeal){
+
+    return null;}
+
 }

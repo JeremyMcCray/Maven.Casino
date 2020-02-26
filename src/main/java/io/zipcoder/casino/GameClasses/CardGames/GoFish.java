eb42e5d5b069d7576c96f49472f8cea7771e65b4
Zanetta Norris <znorris@zipcodes-MBP-2.localdomain>
2020-02-25 19:01:50 -0500

    Added some stubs to GoFishGame class

--- src/main/java/io/zipcoder/casino/GameClasses/CardGames/GoFish.java
+++ src/main/java/io/zipcoder/casino/GameClasses/CardGames/GoFish.java
@@ -1,4 +1,18 @@
 package io.zipcoder.casino.GameClasses.CardGames;
 
+import io.zipcoder.casino.Player.Hand;
+
 public class GoFish {
+Integer dealerScore;
+Integer playerScore;
+public void playGoFish(){}
+public Card dealHand(Hand cardsInPlayerHand){
+    return null;
+    }
+public Card getNextPlay(Card nextPlay){
+    return null;
+    }
+ public Card removePairs(Hand cardsInPlayerHand){
+    return null;
+    }
 }
